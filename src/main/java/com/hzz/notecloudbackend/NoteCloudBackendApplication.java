dc0514b0dd0ea431d33b3bfeb4641461661cd16b
mikucool <q1548701641@gmail.com>
2022-09-21 16:23:51 +0800

    init project

---
+++ src/main/java/com/hzz/notecloudbackend/NoteCloudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hzz.notecloudbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteCloudBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteCloudBackendApplication.class, args);
+    }
+
+}
